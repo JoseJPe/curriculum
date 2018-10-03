beb33d0aaabea26ec68c5f52f9807702ccb6f92a
JoseJPe <42558690+JoseJPe@users.noreply.github.com>
2018-10-03 19:51:48 -0300

    Add files via upload

---
+++ revealed.js
@@ -0,0 +1,15 @@
+
+
+ScrollReveal().reveal('.content',{
+    origin: 'left',
+    distance:'300px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.tip-cont',{
+    origin: 'bottom',
+    distance:'300px',
+    duration: 2000,
+    reset: true
+});
